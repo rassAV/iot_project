894d86dabbfeffdc6c105d906c8100414fcfd947
rassAV <126838987+rassAV@users.noreply.github.com>
2024-05-28 09:27:48 +0800

    Update crud.py

--- server/crud.py
+++ server/crud.py
@@ -5,7 +5,7 @@ import datetime
 
 def get_data(db: Session, esp_id: int = 0):
     results = db.query(models.Data).filter(models.Data.esp_id == esp_id).all()
-    return [i.value for i in results]
+    return [i.pm25 for i in results]
 
 def create_data(db: Session, data: schemas.AirValue):
     db_data = models.Data(**{"pm25": data.pm25, "pm10": data.pm10, "esp_name": data.esp_name, "esp_id": data.esp_id, "timestamp": datetime.datetime.now()})
